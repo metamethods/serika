3f6cf9984f3415ac687ca7fb16b3847c8f397257
__metamethods <tabledmetamethods@gmail.com>
2023-05-05 18:16:33 -0700

    refactor: Removed cooldown from ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -5,7 +5,6 @@ export default new Command({
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Pong!"),
-  cooldown: 5,
   run: async ({ interaction, client }) => {
     const sent = await interaction.reply({ content: "Fetching...", fetchReply: true });
     await interaction.editReply({
