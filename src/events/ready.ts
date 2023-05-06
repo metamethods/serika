f3c172dfe048cba35cfa86e9ef80ca0bd580d607
__metamethods <tabledmetamethods@gmail.com>
2023-05-05 18:10:16 -0700

    refactor: Added a new line after bot is logged in

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,5 +4,5 @@ import Event from "@classes/Event";
 import Info from "@util/Info";
 
 export default new Event('once', Events.ClientReady, (client) => {
-  Info.write(["Final", "#5457ff"], `Logged in as ${client.user!.tag}`);  
+  Info.write(["Final", "#5457ff"], `Logged in as ${client.user!.tag}\n`); 
 });
