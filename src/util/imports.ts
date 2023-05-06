8d1d72fec2da579e284cc617c9c085050b69e1f4
__metamethods <tabledmetamethods@gmail.com>
2023-05-05 19:43:56 -0700

    refactor: Removing commented code

--- src/util/imports.ts
+++ src/util/imports.ts
@@ -2,17 +2,6 @@ export function clearCache(filePath: string) {
   delete require.cache[require.resolve(filePath)];
 }
 
-// export function getFileImportPath(targetPath: string): string {
-//   const relativePath = __dirname.replace(process.cwd(), "");
-//   const upDirectory = "../".repeat(relativePath.split("\\").length - 1);
-
-//   return `${upDirectory}${targetPath.replace(/\\/g, "/")}`;
-// }
-
-// export function removeFileExtension(filePath: string): string {
-//   return filePath.split(".").slice(0, -1).join(".");
-// }
-
 export async function importFile(
   filePath: string,
   reload?: boolean,
