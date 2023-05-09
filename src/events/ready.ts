3e01b80e21e93460fbf9af7ea09d7dd20dcee931
__metamethods <tabledmetamethods@gmail.com>
2023-05-08 18:42:13 -0700

    Rewrote info output in ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,5 +4,6 @@ import Event from "@classes/Event";
 import Info from "@util/Info";
 
 export default new Event('once', Events.ClientReady, (client) => {
-  Info.write(["Final", "#5457ff"], `Logged in as ${client.user!.tag}\n`); 
+  // Info.write(["Final", "#5457ff"], `Logged in as ${client.user!.tag}\n`); 
+  new Info("Client").write(["Final", "#5457ff"], `Logged in as ${client.user!.tag}\n`);
 });
